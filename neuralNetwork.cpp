abfdff31578082fe6a2a28763ccea8a68b06708c
Gaurang-2005 <149991661+Gaurang-2005@users.noreply.github.com>
2026-02-28 17:59:43 +0530

    Complete backpropagation code!!

--- neuralNetwork.cpp
+++ neuralNetwork.cpp
@@ -3,20 +3,34 @@
 #include <stdexcept>
 #include <utility>
 #include <vector>
+#include <random>
 #include <iostream>
 
 enum class activationFunction {ReLU, sigmoid, Linear};
+std::random_device rd;
+std::mt19937 rng(rd());
 
 class neuron {
 public:
     int nInputs;
     std::vector<float> weights;
     float bias;
-    neuron(int prevLayers) : nInputs{prevLayers}, bias(0.5) {
-
-        for (int i = 0; i < nInputs; i++) {
-            weights.push_back(0.5);
+    neuron(int prevLayers, activationFunction a) : nInputs{prevLayers}, bias{0.01} {
+        if (a == activationFunction::sigmoid || a == activationFunction::Linear) {
+            float limit = std::sqrt(3.0f / nInputs);
+            std::uniform_real_distribution<float> dist(-limit, limit);
+            for (int i = 0; i < nInputs; i++) {
+                weights.push_back(dist(rng));
+            }
         }
+        else {
+            float limit = std::sqrt(6.0f / nInputs);
+            std::uniform_real_distribution<float> dist(-limit, limit);
+            for (int i = 0; i < nInputs; i++) {
+                weights.push_back(dist(rng));
+            }
+        }
+        
     }
 
     std::pair<float, float> activate(activationFunction a, const std::vector<float>& inputs) const {
@@ -38,10 +52,11 @@ public:
     }
     float activationDer(activationFunction a, float sum) {
         switch (a) {
-            case activationFunction::Linear: return LinearDer(sum);
+            case activationFunction::Linear: return LinearDer();
             case activationFunction::ReLU: return ReLUDer(sum);
             case activationFunction::sigmoid: return sigmoidDer(sum);
         }
+        return 0;
     }
     float Linear(float sum) const {
         return sum;
@@ -55,7 +70,7 @@ public:
         }
         else return 0;
     }
-    float LinearDer(float sum) const {
+    float LinearDer() const {
         return 1;
     }
     float ReLUDer(float sum) const {
@@ -66,8 +81,9 @@ public:
         float activ = sigmoid(sum);
         return activ * (1 - activ);
     }
-    float sumDer(const int& i, const std::vector<std::vector<float>>& inputs, const int& n1) const {
-        if (i < int(weights.size())) return inputs[n1 - 1][i];
+    float sumDer(const int& i, const std::vector<float>& inputs) const {
+
+        if (i < int(weights.size())) return inputs[i];
         else if (i == int(weights.size())) return 1;
         else assert(false);
         return 0;
@@ -81,7 +97,7 @@ public:
     std::vector<neuron> neuronLayer;
     layer(int n, int in, activationFunction b) : nInputs(in), a(b) {
         for (int i = 0; i < n; i++) {
-            neuronLayer.push_back(neuron(in));
+            neuronLayer.push_back(neuron(in, a));
         }
     }
     std::pair<std::vector<float>, std::vector<float>> forward(const std::vector<float>& inputs) const {
@@ -152,7 +168,7 @@ public:
             for (int i = 0; i < int(inputs.size()); i++) {
                 pred.push_back(predict(inputs[i]));
             }
-            std::cout<<"dataset MSE at epoch " << ep << ": "<< MSE(pred, target);
+            std::cout<<"dataset MSE at epoch " << ep << ": "<< MSE(pred, target)<<std::endl;
             for (int i = 0; i < int(inputs.size()); i++) {
                 std::pair<std::vector<std::vector<float>>,std::vector<std::vector<float>>> out(forward(inputs[i]));
                 std::vector<std::vector<float>> sum = out.first;
@@ -161,13 +177,20 @@ public:
                     throw std::length_error("the size of output of neural network and size of expected values is not same!!");
                 std::vector<std::vector<std::vector<float>>> updatedWeights;
                 std::vector<std::vector<float>> layerWeightsTemp;
+                std::vector<std::vector<float>> delta(activ);
                 for (int n2 = int(activ[int(activ.size() - 1)].size()) - 1; n2 >= 0; n2--) {
-                    float errorDer = MSE_der(activ[int(activ.size() - 1)][n2], target[i][n2], activ[int(activ.size() - 1)].size());
+                    float errorDer = MSE_der(activ[int(activ.size() - 1)][n2], target[i][n2], int(activ[int(activ.size() - 1)].size()));
                     neuron temp(layers[int(activ.size() - 1)].neuronLayer[n2]);
                     float activDer = temp.activationDer(layers[int(activ.size() - 1)].a, sum[int(activ.size() - 1)][n2]);
+                    delta[int(activ.size() - 1)][n2] = errorDer * activDer;
                     std::vector<float> updatedWeight;
                     for (int nw = 0; nw <= temp.nInputs; nw++) {
-                        float sumDer = temp.sumDer(nw, activ, int(activ.size() - 1));
+                        float sumDer;
+                        if (!(int(activ.size() - 2) < 0))
+                            sumDer = temp.sumDer(nw, activ[int(activ.size() - 2)]);
+                        else {
+                            sumDer = temp.sumDer(nw, inputs[i]);
+                        }
                         if (nw < temp.nInputs)
                             updatedWeight.push_back(temp.weights[nw] - learningRate * errorDer * activDer * sumDer);
                         else updatedWeight.push_back(temp.bias - learningRate * errorDer * activDer * sumDer);
@@ -177,16 +200,38 @@ public:
                 }
                 updatedWeights.push_back(layerWeightsTemp);
                 for (int n1 = int(activ.size()) - 2; n1 >= 0; n1--) {
+                    std::vector<std::vector<float>> layerWeights;
                     for (int n2 = int(activ[n1].size()) - 1; n2 >= 0; n2--) {
-                        float der {0};
+                        float tempErrorDer {0};
+                        neuron temp(layers[n1].neuronLayer[n2]);
+                        for (int k = 0; k < int(sum[n1 + 1].size()); k++) {
+                            tempErrorDer += layers[n1 + 1].neuronLayer[k].weights[n2] * delta[n1 + 1][k];
+                        }
+                        float activDer = temp.activationDer(layers[n1].a, sum[n1][n2]);
+                        float errorDer = tempErrorDer;
+                        std::vector<float> updatedWeight;
+                        delta[n1][n2] = errorDer * activDer;
+                        for (int nw = 0; nw <= temp.nInputs; nw++) {
+                            float sumDer;
+                            if (!(n1 - 1 < 0))
+                                sumDer = temp.sumDer(nw, activ[n1 -1]);
+                            else {
+                                sumDer = temp.sumDer(nw, inputs[i]);
+                            }
+                            if (nw < temp.nInputs)
+                                updatedWeight.push_back(temp.weights[nw] - learningRate * errorDer * activDer * sumDer);
+                            else updatedWeight.push_back(temp.bias - learningRate * errorDer * activDer * sumDer);
 
+                        }
+                        layerWeights.push_back(updatedWeight);
 
                     }
+                    updatedWeights.push_back(layerWeights);
                 }
                 for (int layerI = 0; layerI < int(updatedWeights.size()); layerI++) {
                     for (int neuronI = 0; neuronI < int(updatedWeights[layerI].size()); neuronI++) {
                         for (int weightI = 0; weightI < int(updatedWeights[layerI][neuronI].size()); weightI++) {
-                            if (weightI < layers[int(updatedWeights.size()) - layerI - 1].neuronLayer[int(updatedWeights[layerI].size()) - 1 - neuronI].weights.size()) {
+                            if (weightI < int(layers[int(updatedWeights.size()) - layerI - 1].neuronLayer[int(updatedWeights[layerI].size()) - 1 - neuronI].weights.size())) {
                                 layers[int(updatedWeights.size()) - layerI - 1].neuronLayer[int(updatedWeights[layerI].size()) - 1 - neuronI].weights[weightI] = updatedWeights[layerI][neuronI][weightI];
                             }
                             else layers[int(updatedWeights.size()) - layerI - 1].neuronLayer[int(updatedWeights[layerI].size()) - 1 - neuronI].bias = updatedWeights[layerI][neuronI][weightI];
@@ -197,4 +242,3 @@ public:
         }
     }
 };
-
